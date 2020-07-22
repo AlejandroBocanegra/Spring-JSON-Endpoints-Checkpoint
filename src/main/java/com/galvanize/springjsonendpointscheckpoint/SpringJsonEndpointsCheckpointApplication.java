1362abc8a428ed9f3936855bc3fdbb3439bfcccc
AlejandroBocanegra <a.boca@outlook.com>
2020-07-22 23:25:11 +0900

    initial submit

---
+++ src/main/java/com/galvanize/springjsonendpointscheckpoint/SpringJsonEndpointsCheckpointApplication.java
@@ -0,0 +1,13 @@
+package com.galvanize.springjsonendpointscheckpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJsonEndpointsCheckpointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJsonEndpointsCheckpointApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/galvanize/springjsonendpointscheckpoint/SpringJsonEndpointsCheckpointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galvanize.springjsonendpointscheckpoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJsonEndpointsCheckpointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
